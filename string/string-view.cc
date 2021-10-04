d790d5f47e581aa0cfd293ad76d23927532fe4da
Conzxy <1967933844@qq.com>
2021-10-04 23:24:57 +0800

    move to kanon

--- string/string-view.cc
+++
@@ -1,7 +0,0 @@
-#include "string-view.h"
-
-namespace kanon {
-	
-constexpr StringView::size_type StringView::npos;
-
-} // namespace kanon
--- string/test/1.cc
+++
@@ -1,9 +0,0 @@
-#include <string_view>
-#include <stdio.h>
-using namespace std::literals;
-
-int main() {
-	auto x = "aaaaa"sv.find('a');
-	
-	printf("%d", x);
-}
