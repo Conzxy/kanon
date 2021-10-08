67a36cb1f68a6691e0aa456995b037a53a8025bc
Conzxy <Conzxy@1967933844@qq.com>
2021-10-08 10:57:32 +0800

    add restart() 	modified:   kanon/time/TimeStamp.h

--- kanon/net/timer/Timer.cc
+++ kanon/net/timer/Timer.cc
@@ -4,4 +4,12 @@ namespace kanon {
 
 AtomicInt64 Timer::s_counter_ {};
 
+void
+Timer::restart(TimeStamp now) KANON_NOEXCEPT {
+	if (repeat_) {
+		expiration_ = addTime(now, interval_);
+	} else {
+		expiration_.
+	}
+}
 } // namespace kanon
