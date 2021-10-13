f44edec025ffdb2c6b7eba3934229828bbdc0fcd
Conzxy <Conzxy@1967933844@qq.com>
2021-10-13 20:33:17 +0800

    add ENABLE_EPOLL

--- kanon/util/macro.h
+++ kanon/util/macro.h
@@ -5,10 +5,33 @@
 #define KANON_RETHROW throw
 
 #if __cplusplus >= 201103L
-#define KANON_NOEXCEPT noexcept
-#else
-#define KANON_NOEXCEPT throw()
+#define CXX_STANDARD_11
 #endif
 
+#if __cplusplus >= 201402L
+#define CXX_STANDARD_14
+#endif
+
+#if __cplusplus >= 201703L
+#define CXX_STANDARD_17
+#endif
+
+#if __cplusplus < 201103L
+#define CXX_STANDARD_LESS_THAN_11
+#endif
+
+#ifdef CXX_STANDARD_11
+#define KANON_NOEXCEPT noexcept
+#define KANON_OVERRIDE override
+#else
+#define KANON_NOEXCEPT throw()
+#define KANON_OVERRIDE
+#endif
+
+#ifdef CXX_STANDARD_14
+#define KANON_CONSTEXPR constexpr
+#else
+#define KANON_CONSTEXPR inline
+#endif
 
 #endif // KANON_UTIL_MACRO_H
