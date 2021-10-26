9d7001d5fb8ed162ba5cba13a4662b4751fa9f9a
Conzxy <Conzxy@1967933844@qq.com>
2021-10-26 18:41:05 +0800

    change name style

---
+++ kanon/thread/current_thread.h
@@ -0,0 +1,42 @@
+#ifndef KANON_CURRENT_THREAD_H
+#define KANON_CURRENT_THREAD_H
+
+#include "kanon/util/macro.h"
+
+namespace kanon {
+
+// __thread requires file scope(i.e. global scope)
+// so use namespace instead of class
+namespace CurrentThread {
+// __thread can declare thread-independent variable(only POD-type)
+//
+// for logging
+extern __thread int t_tid;
+extern __thread char t_tidString[32]; 
+extern __thread int t_tidLength;
+extern __thread char const* t_name;
+
+void cacheTid() KANON_NOEXCEPT;
+
+inline int tid() KANON_NOEXCEPT
+{
+	if( __builtin_expect(t_tid == 0, 0) )
+		cacheTid();
+	return t_tid;
+}
+
+inline char const* tidString() KANON_NOEXCEPT
+{ return t_tidString; }
+
+inline int tidLength() KANON_NOEXCEPT
+{ return t_tidLength; }
+
+inline char const* name() KANON_NOEXCEPT
+{ return t_name; }
+
+bool isMainThread() KANON_NOEXCEPT;
+
+} // namespace CurrentThread
+} // namespace kanon
+
+#endif // KANON_CURRENT_THREAD_H
