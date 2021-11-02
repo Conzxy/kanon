8d9a740de0a4eeb3b56a9cae8fb541c184732229
Conzxy <Conzxy@1967933844@qq.com>
2021-11-02 17:09:19 +0800

    a convenient header

---
+++ kanon/net/common.h
@@ -0,0 +1,11 @@
+#ifndef KANON_NET_COMMON_H
+#define KANON_NET_COMMON_H
+
+// The file is just collection of some header
+// that are very likely included by user
+#include "kanon/net/EventLoop.h"
+#include "kanon/net/TcpConnection.h"
+#include "kanon/net/Buffer.h"
+#include "kanon/net/InetAddr.h"
+
+#endif // KANON_NET_COMMON_H
