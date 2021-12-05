c03887b19aeb9279dbe0183ec81dbea37841b7d9
Conzxy <Conzxy@1967933844@qq.com>
2021-12-05 11:20:00 +0800

    仿rust风格算术类型别名

---
+++ kanon/util/arithmetic_type.h
@@ -0,0 +1,33 @@
+#ifndef KANON_UTIL_ARITHMETIC_H
+#define KANON_UTIL_ARITHMETIC_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef intmax_t imax;
+typedef uintptr_t iptr;
+
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+typedef unsigned long ulong;
+typedef unsigned long long ullong;
+
+typedef size_t isize;
+typedef ssize_t usize;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef uintmax_t umax;
+
+typedef float f32;
+typedef double f64;
+typedef long double lf64; // FIXME f96?
+
+#endif // KANON_UTIL_ARITHMETIC_H
