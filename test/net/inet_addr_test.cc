b78c598023470c8a867090128856ffc165671fe2
Conzxy <Conzxy@1967933844@qq.com>
2022-04-19 13:36:47 +0800

    update

--- test/net/inet_addr_test.cc
+++ test/net/inet_addr_test.cc
@@ -11,7 +11,15 @@ TEST(InetAddr, resolve) {
     std::cout << addr.ToIpPort() << '\n';
   }
 
-  addrs = InetAddr::Resolve("172.26.34.206", "http");
+  std::cout << "=====" << "Client resolve" << "=====\n";
+  addrs = InetAddr::Resolve("www.baidu.com", "http");
+
+  for (auto& addr : addrs) {
+    std::cout << addr.ToIpPort() << "\n";
+  }
+
+  std::cout << "=====" << "Server resolve" << "=====\n";
+  addrs = InetAddr::Resolve("www.baidu.com", "80", InetAddr::kServer);
 
   for (auto& addr : addrs) {
     std::cout << addr.ToIpPort() << "\n";
@@ -22,6 +30,18 @@ TEST(InetAddr, ctor) {
   InetAddr addr("www.baidu.com", "http");
 
   std::cout << addr.ToIpPort() << "\n";
+
+  InetAddr addr2("127.0.0.1:80");
+
+  std::cout << addr2.ToIpPort() << "\n";
+
+  InetAddr addr3("[3ff2::0:8:1fff]:80");
+
+  std::cout << addr3.ToIpPort() << "\n";
+
+  InetAddr addr4("www.baidu.com:80");
+
+  std::cout << addr4.ToIpPort() << "\n";
 }
 
 int main(int argc, char* argv[]) {
