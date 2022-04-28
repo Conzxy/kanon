8e19804ae29cf62d099c094b63a339c6d6b3e929
Conzxy <Conzxy@1967933844@qq.com>
2022-04-28 21:10:17 +0800

    add: allow more config

---
+++ kanon/log/log_file.cc
@@ -0,0 +1,156 @@
+#include "log_file.h"
+
+namespace kanon {
+
+template<bool T>
+LogFile<T>::LogFile(StringView basename,
+                    size_t roll_size, 
+                    StringView prefix,
+                    size_t log_file_num,
+                    size_t roll_interval,
+                    size_t flush_interval)
+  : basename_(basename.ToString())
+  , roll_size_(roll_size)
+  , start_of_period_(0)
+  , roll_interval_(roll_interval)
+  , last_roll_(0)
+  , last_flush_(0)
+  , flush_interval_(static_cast<time_t>(flush_interval))
+  , lock_()
+  , prefix_(prefix.ToString())
+  , log_file_num_(log_file_num)
+  , remove_thr_("RemoveLogFile")
+  , remove_cond_(remove_mtx_)
+  , quit_remove_thr_(false)
+{
+  assert(basename.find('/') == StringView::npos);
+  Logger::SetColor(false);
+  RollFile();
+
+  if (log_file_num_ != UINT_MAX) {
+    remove_thr_.StartRun([this]() {
+      while (!quit_remove_thr_) {
+        {
+          MutexGuard guard(remove_mtx_);
+          // There is one consumer only, don't use while is also ok
+          if (log_files_dup_.size() < log_file_num_ - 1) {
+            remove_cond_.Wait();
+          }
+        }
+
+        for (auto log_file : log_files_dup_) {
+          ::remove(log_file.c_str());
+        }
+        log_files_dup_.clear();
+      }
+    });
+  }
+}
+
+template<bool T>
+inline LogFile<T>::~LogFile() noexcept {
+  if (log_file_num_ != UINT_MAX) {
+    quit_remove_thr_ = true;
+    remove_cond_.Notify();
+
+    remove_thr_.Join();
+  }
+}
+
+template<bool T>
+void LogFile<T>::Append(char const* data, size_t num) noexcept {
+  MutexGuardT<MutexPolicy> guard(lock_);
+  
+  file_->Append(data, num);
+
+  if (file_->writtenBytes() > roll_size_) {
+    RollFile();
+  } else {
+    // second precision
+    auto now = ::time(NULL);
+
+    // \waring
+    // To conform the semantic of time_t,
+    // it is not recommended to use the number of days instead of seconds,
+    // but it is only used here.
+    time_t this_period = now / roll_interval_; //* roll_interval;
+
+    // Over the roll_interval_, roll new file
+    if (this_period > start_of_period_) {
+      RollFile();
+    } else if (now - last_flush_ > flush_interval_) {
+      last_flush_ = now;
+      file_->Flush();
+    }
+  }
+
+  if (log_files_.size() >= log_file_num_) {
+    auto last_file = std::move(log_files_.back());
+    log_files_.pop_back();
+
+    log_files_dup_.swap(log_files_);
+    log_files_.emplace_back(std::move(last_file));
+    
+    MutexGuard guard(remove_mtx_);
+    remove_cond_.Notify();
+  }
+}
+
+template<bool T>
+void LogFile<T>::Flush() noexcept {
+  MutexGuardT<MutexPolicy> guard(lock_);
+  file_->Flush();
+}
+
+template<bool T>
+void LogFile<T>::RollFile() {
+  time_t now = ::time(NULL);
+  time_t new_start_period = now / roll_interval_;
+
+  if (now > last_roll_) {
+    auto filename = GetLogFilename(now);
+    last_roll_ = now;
+    last_flush_ = now;
+    start_of_period_ = new_start_period;
+
+    file_.reset(new AppendFile(filename));
+    log_files_.emplace_back(filename);
+  }
+}
+
+template<bool T>
+std::string LogFile<T>::GetLogFilename(time_t& now) {
+  // Log file name format:
+  // basename.timestamp.pid.hostname.log
+  std::string filename;
+
+  filename.reserve(basename_.size()+prefix_.size()+128);
+
+  filename += prefix_.data();
+
+  if (!prefix_.empty() && !(prefix_.back() == '/')) {
+    filename += '/';
+  }
+
+  filename += basename_.data();
+
+  char timebuf[32];
+  struct tm tm;
+  ::tzset();
+  ::localtime_r(&now, &tm);
+
+  ::strftime(timebuf, sizeof timebuf, ".%Y%m%d-%H%M%S.", &tm);
+
+  filename += timebuf;
+  filename += process::PidString().data();
+  filename += ".";
+  filename += process::Hostname().data();
+  filename += ".log";
+
+  return filename;
+}
+
+template class LogFile<true>;
+template class LogFile<>;
+
+} // namespace kanon
--- kanon/log/log_file.h
+++ kanon/log/log_file.h
@@ -5,13 +5,19 @@
 #include <time.h>
 #include <sys/time.h>
 #include <string>
+#include <limits.h>
 #include <type_traits> // std::conditional
 
 #include "kanon/util/noncopyable.h"
 #include "kanon/util/macro.h"
+#include "kanon/util/measure_time.h"
 #include "kanon/string/string_view.h"
+
 #include "kanon/thread/dummy_mutex_lock.h"
 #include "kanon/thread/mutex_lock.h"
+#include "kanon/thread/thread.h"
+#include "kanon/thread/condition.h"
+
 #include "kanon/process/process_info.h"
 
 #include "kanon/log/logger.h"
@@ -20,162 +26,81 @@
 namespace kanon {
 
 /**
- * \brief just roll file and write to file
+ * \brief Append log to file
+ * 
+ * The file is rolled automatically when the filesize reach threshold
+ * or new day
  * \note should be used by Logger
  */
 template<bool ThreadSafe = false>
-class LogFile : noncopyable
-{
+class LogFile : noncopyable {
 public:
+
+  /**
+   * \param basename Filename of this process
+   * \param roll_size File size to roll new file
+   * \param prefix Prefix of directory name that store log files(default: current directory)
+   * \param log_file_num The threshold to remove old log files(default: UINT_MAX, i.e. don't remove)
+   * \param roll_interval Interval to roll new file in seconds(default: 86400, i.e. 1 day)
+   * \param flush_interval Interval of flushing contents in buffer to file in seconds(default: 3s)
+   */
   LogFile(StringView basename, 
-      size_t rollSize, 
-      StringView prefix = StringView{ },
-      size_t flushInterval = 3,
-      size_t rollLine = 1024);
+          size_t roll_size, 
+          StringView prefix = StringView{ },
+          size_t log_file_num = UINT_MAX,
+          size_t roll_interval = kRollPerSeconds_,
+          size_t flush_interval = 3);
 
   ~LogFile() noexcept;
   
   void Append(char const* data, size_t num) noexcept;
-  void flush() noexcept;
+  void Flush() noexcept;
 private:
-  void rollFile();
-  static std::string formatTime() noexcept;
-  std::string getLogFileName(time_t& now);  
+  void RollFile();
+  std::string GetLogFilename(time_t& now);  
 
+  static std::string FormatTime() noexcept;
 private:
-  StringView basename_;
-  size_t rollSize_; // when file size > rollSize, roll new file
-  size_t rollLine_; // when file line count > rollLine, also roll new file
-  size_t countLine_; // count of written line
+  std::string basename_; //!< The basename of process
+  size_t roll_size_; //!< when file size > rollSize, roll new file
 
-  time_t startOfPeriod_; // over the start of period, roll new file
-  time_t lastRoll_; // remember last roll time to avoid roll same file at the same time
-  time_t lastFlush_;
-  time_t flushInterval_; // if now - lastFlush > flushInterval, flush buffer to file
+  time_t start_of_period_; //!< over the start of period, roll new file
+  time_t roll_interval_;
+  time_t last_roll_; //!< remember last roll time to avoid roll same file at the same time
+
+  time_t last_flush_;
+  time_t flush_interval_; //!< if now - lastFlush > flushInterval, flush buffer to file
   
   // lock or dummy lock
-  typedef typename std::conditional<ThreadSafe, MutexLock, DummyMutexLock>::type 
-    MutexPolicy;
+  using MutexPolicy = typename std::conditional<ThreadSafe, MutexLock, DummyMutexLock>::type;
 
   MutexPolicy lock_;
   std::unique_ptr<AppendFile> file_;
   
-  // FIXME std::string? 
-  StringView prefix_;
+  std::string prefix_; //!< directory that store log file
 
-  // seconds for one day
+  size_t log_file_num_;
+  std::vector<std::string> log_files_;
+  std::vector<std::string> log_files_dup_;
+  Thread remove_thr_;
+  Condition remove_cond_;
+  MutexLock remove_mtx_;
+  bool quit_remove_thr_;
+
   static constexpr uint32_t kRollPerSeconds_ = 24 * 60 * 60; //or 86400
 };
 
-template<bool T>
-LogFile<T>::LogFile(StringView basename,
-         size_t rollSize, 
-         StringView prefix,
-         size_t flushInterval,
-         size_t rollLine)
-  : basename_(basename)
-  , rollSize_(rollSize)
-  , rollLine_(rollLine)
-  , countLine_(0)
-  , startOfPeriod_(0)
-  , lastRoll_(0)
-  , lastFlush_(0)
-  , flushInterval_(static_cast<time_t>(flushInterval))
-  , lock_()
-  , prefix_(prefix)
-{
-  assert(basename.find('/') == StringView::npos);
-  Logger::SetColor(false);
-  rollFile();  
+template<bool ThreadSafe>
+void SetupLogFile(LogFile<ThreadSafe>& lf) {
+  Logger::SetOutputCallback([&lf](char const* data, size_t num) {
+    lf.Append(data, num);
+  });
+
+  Logger::SetFlushCallback([&lf]() {
+    lf.Flush();
+  });
 }
 
-template<bool T>
-inline LogFile<T>::~LogFile() noexcept = default;
-
-template<bool T>
-void LogFile<T>::Append(char const* data, size_t num) noexcept {
-  MutexGuardT<MutexPolicy> guard(lock_);
-  
-  file_->Append(data, num);
-
-  if (file_->writtenBytes() > rollSize_)
-    rollFile();
-  else {
-    ++countLine_;
-    if (countLine_ >= rollLine_) {
-      countLine_ = 0;
-      // second precision
-      auto now = ::time(NULL);
-  
-      // @waring
-      // To conform the semantic of time_t,
-      // it is not recommended use the number of days instead of seconds,
-      // but it is only used here.
-      time_t thisPeriod = now / kRollPerSeconds_; //* kRollPerSeconds;
-
-      if (thisPeriod > startOfPeriod_) {
-        rollFile();
-      } else if (now - lastFlush_ > flushInterval_) {
-        lastFlush_ = now;
-        file_->flush();
-      }
-    }
-  }
-}
-
-template<bool T>
-void LogFile<T>::flush() noexcept {
-  MutexGuardT<MutexPolicy> guard(lock_);
-  file_->flush();
-}
-
-template<bool T>
-void LogFile<T>::rollFile() {
-  time_t now;  
-  auto filename = getLogFileName(now);
-  
-  time_t newStartPeriod = now / kRollPerSeconds_;// * kRollPerSeconds_;
-  if (now > lastRoll_) {
-    lastRoll_ = now;
-    lastFlush_ = now;
-    startOfPeriod_ = newStartPeriod;
-
-    file_.reset(new AppendFile(filename));
-  }
-}
-
-template<bool T>
-std::string LogFile<T>::getLogFileName(time_t& now) {
-  // Log file name format:
-  // basename.timestamp.pid.hostname.log
-  std::string filename;
-
-  filename.reserve(basename_.size()+prefix_.size()+128);
-
-  filename += prefix_.data();
-
-  if (!prefix_.empty() && !prefix_.ends_with('/')) {
-    filename += '/';
-  }
-
-  filename += basename_.data();
-
-  char timebuf[32];
-  struct tm tm;
-  now = ::time(NULL);
-  tzset();
-  ::localtime_r(&now, &tm);
-
-  ::strftime(timebuf, sizeof timebuf, ".%Y%m%d-%H%M%S.", &tm);
-
-  filename += timebuf;
-  filename += process::PidString().data();
-  filename += ".";
-  filename += process::Hostname().data();
-  filename += ".log";
-
-  return filename;
-}
 
 } // namespace kanon
 
