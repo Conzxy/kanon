6a8bbc7be6ea1c62b2aaca36e1d16946f615539c
Conzxy <1967933844@qq.com>
2022-05-14 18:04:28 +0800

    add: handler of SIGINT,SIGTERM,SIGKILL

--- kanon/net/tcp_server.cc
+++ kanon/net/tcp_server.cc
@@ -1,5 +1,6 @@
 #include "kanon/net/tcp_server.h"
 
+#include "kanon/util/macro.h"
 #include "kanon/util/ptr.h"
 
 #include "kanon/net/inet_addr.h"
@@ -7,9 +8,37 @@
 #include "kanon/net/event_loop.h"
 #include "kanon/net/event_loop_pool.h"
 #include "kanon/net/acceptor.h"
+
+#include <signal.h>
 #include <atomic>
 
 using namespace kanon;
+
+EventLoop* g_loop = nullptr;
+
+inline void SignalHandler(int signo, int expected, char const* msg) noexcept {
+  assert(signo == expected);
+  LOG_INFO << msg;
+
+  if (g_loop) {
+    LOG_INFO << "The event loop is quiting...";
+    g_loop->Quit();
+  }
+
+  LOG_INFO << "The server exit successfully";
+}
+
+inline void SigKillHandler(int signo) noexcept {
+  SignalHandler(signo, SIGKILL, "Catching SIGKILL, force kill the server");
+}
+
+inline void SigIntHandler(int signo) noexcept {
+  SignalHandler(signo, SIGINT, "Catching SIGINT(i.e. User type Ctrl+c), server is exiting");
+}
+
+inline void SigTermHandler(int signo) noexcept {
+  SignalHandler(signo, SIGTERM, "Catching SIGTERM(e.g. kill(1)), server is terminated");
+}
 
 TcpServer::TcpServer(EventLoop* loop,
                      InetAddr const& listen_addr,
@@ -24,6 +53,11 @@ TcpServer::TcpServer(EventLoop* loop,
   , start_once_{ ATOMIC_FLAG_INIT }
 {
   SetConnectionCallback(&DefaultConnectionCallback);
+
+  g_loop = loop_;
+  ::signal(SIGINT, &SigIntHandler);
+  ::signal(SIGTERM, &SigTermHandler);
+  ::signal(SIGKILL, &SigKillHandler);
 
   acceptor_->SetNewConnectionCallback([this](int cli_sock, InetAddr const& cli_addr) {
     // Ensure in main thread
@@ -52,7 +86,7 @@ TcpServer::TcpServer(EventLoop* loop,
       auto io_loop = conn->GetLoop();
       io_loop->AssertInThread();
 
-      int n = 0;
+      int n = 0; KANON_UNUSED(n);
       {
       MutexGuard guard(lock_conn_);
       n = connections_.erase(conn->GetName());
@@ -104,7 +138,7 @@ void TcpServer::StartRun() noexcept {
     // start IO loop
     pool_->StartRun();
     loop_->RunInLoop([this]() {
-      LOG_TRACE << "Listening in " << ip_port_;
+      LOG_TRACE_KANON << "Listening in " << ip_port_;
       acceptor_->Listen();
     });
   }
