5767d3059b5badb4a648374e85ab1d6e5bbc9bff
Conzxy <1967933844@qq.com>
2022-05-14 18:08:52 +0800

    update

--- kanon/rpc/krpc_client.h
+++ kanon/rpc/krpc_client.h
@@ -1,1 +1,46 @@
-#include "kanon/rpc/internal/krpc_client_impl.h"
+#ifndef KANON_NET_KRPC_CLIENT_H
+#define KANON_NET_KRPC_CLIENT_H
+
+#include "kanon/net/user_client.h"
+#include "kanon/thread/condition.h"
+#include "kanon/thread/mutex_lock.h"
+#include "kanon/thread/count_down_latch.h"
+#include "kanon/rpc/krpc_channel.h"
+#include "kanon/util/ptr.h"
+
+namespace kanon {
+namespace protobuf {
+namespace rpc {
+
+template<typename Stub>
+class KRpcClient : public TcpClient {
+public:
+  using StubPtr = std::shared_ptr<Stub>;
+
+  KRpcClient(EventLoop* loop, InetAddr const& addr, std::string const& name);
+
+  StubPtr GetStub() const;
+  
+  CountDownLatch& GetLatch() const noexcept 
+  { 
+    if (latch_ == nullptr) {
+      latch_= kanon::make_unique<CountDownLatch>(1);
+    }
+    return *latch_;
+  }
+
+  void OnConnection(TcpConnectionPtr const& conn);
+private:
+
+  mutable std::unique_ptr<CountDownLatch> latch_;
+  mutable MutexLock connected_mutex_;
+  mutable Condition connected_cond_;
+};
+
+} // namespace rpc
+} // namespace protobuf
+} // namespace kanon
+
+#include "kanon/rpc/krpc_client_impl.h"
+
+#endif
