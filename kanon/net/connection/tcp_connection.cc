64b55bf20e601f2bb2fb9cdee78d90317caae206
Conzxy <Conzxy@1967933844@qq.com>
2022-06-11 16:14:42 +0800

    reuse connection API

---
+++ kanon/net/connection/tcp_connection.cc
@@ -0,0 +1,70 @@
+#include "tcp_connection.h"
+
+#include "kanon/net/socket.h"
+
+using namespace std;
+using namespace kanon;
+
+
+// TcpConnection::TcpConnection(EventLoop*  loop,
+//                std::string const& name,
+//                int sockfd,
+//                InetAddr const& local_addr,
+//                InetAddr const& peer_addr)
+//   : loop_{ loop }
+//   , name_{ name }
+//   , socket_{ kanon::make_unique<Socket>(sockfd) }
+//   , channel_{ kanon::make_unique<Channel>(loop, sockfd) }
+//   , local_addr_{ local_addr }
+//   , peer_addr_{ peer_addr }
+//   , high_water_mark_{ kDefaultHighWatermark }
+//   , state_{ kConnecting }
+// {
+//   // Pass raw pointer is safe here since 
+//   // will disable all events when connection
+//   // become disconnectioned(later, it will
+//   // be destroyed)
+//   channel_->SetReadCallback(std::bind(
+//     &TcpConnection::HandleRead, this, _1));
+
+//   channel_->SetWriteCallback(std::bind(
+//     &TcpConnection::HandleWrite, this));
+
+//   channel_->SetErrorCallback(std::bind(
+//     &TcpConnection::HandleError, this));
+  
+//   channel_->SetCloseCallback(std::bind(
+//     &TcpConnection::HandleClose, this));
+
+//   LOG_TRACE_KANON << "TcpConnection::ctor [" << name_ << "] created";
+  
+// }
+
+// TcpConnection::~TcpConnection() noexcept {
+//   assert(state_ == kDisconnected);
+//   LOG_TRACE_KANON << "TcpConnection::dtor [" << name_ << "] destroyed";
+// }
+
+
+TcpConnection::TcpConnection(EventLoop* loop, 
+                             string const& name,
+                             int sockfd,
+                             InetAddr const& local_addr,
+                             InetAddr const& peer_addr)
+  : Base(loop, name, sockfd)
+  , local_addr_(local_addr)
+  , peer_addr_(peer_addr)
+{
+  LOG_TRACE_KANON << "TcpConnection::ctor [" << name_ << "] created";
+}
+
+TcpConnection::~TcpConnection() noexcept {
+  LOG_TRACE_KANON << "TcpConnection::dtor [" << name_ << "] destroyed";
+}
+
+void TcpConnection::SetNoDelay(bool flag) noexcept
+{ socket_->SetNoDelay(flag); }
+
+void TcpConnection::SetKeepAlive(bool flag) noexcept
+{ socket_->SetKeepAlive(flag); }
+
---
+++ kanon/net/connection/tcp_connection.h
@@ -0,0 +1,96 @@
+#ifndef KANON_NET_TCPCONNECTION_H
+#define KANON_NET_TCPCONNECTION_H
+
+#include "kanon/util/noncopyable.h"
+#include "kanon/util/macro.h"
+#include "kanon/util/ptr.h"
+#include "kanon/util/any.h"
+#include "kanon/log/logger.h"
+
+#include "kanon/net/callback.h"
+#include "kanon/net/inet_addr.h"
+#include "kanon/net/buffer.h"
+#include "connection_base.h"
+
+namespace kanon {
+
+//! \addtogroup net
+//!@{
+
+/**
+ * \brief Represents a tcp connection.
+ *
+ * User don't care detail of it, the work just to register callback as following:
+ *  - Message callback to process message from the peer
+ *  - Highwatermark callback to do something when output buffer is too full
+ *  - Connection callback to do something when connection down or up
+ *  - Write complete callback to do something when write complete
+ *
+ * Besides,
+ *  - Send message to peer(std::string, kanon::StringView, char const*, kanon::Buffer)
+ *  - Shutdown in write direction and close
+ *  - Set context that tie with this connection
+ *
+ * \note 
+ *   Public class
+ */
+class TcpConnection : public ConnectionBase<TcpConnection> {
+  using Base = ConnectionBase<TcpConnection>;
+public:
+  ~TcpConnection() noexcept;
+
+  /**
+   * Shouldn't call this. To call NewTcpConnection(). \n
+   * Can't set this be private member
+   * since std::make_shared<> need access ctor
+   */
+  TcpConnection(EventLoop* loop,
+                std::string const& name,
+                int sockfd,
+                InetAddr const& local_addr,
+                InetAddr const& peer_addr);
+
+  /**
+   * \brief Create a TcpConnection instance correctly
+   * 
+   * This is a factory method
+   * \param loop owner event loop
+   * \param sockfd managed socket fd
+   * \param local_addr local address
+   * \param peer_addr peer or remote address
+   */
+  static TcpConnectionPtr NewTcpConnection(EventLoop* loop, 
+                                           std::string const& name,
+                                           int sockfd,
+                                           InetAddr const& local_addr,
+                                           InetAddr const& peer_addr)
+
+  { return std::make_shared<TcpConnection>(loop, name, sockfd, local_addr, peer_addr); }
+
+  //! Whether disable Negele algorithm
+  void SetNoDelay(bool flag) noexcept;
+  //! Whether disable keep-alive timer
+  void SetKeepAlive(bool flag) noexcept;
+
+  InetAddr const& GetLocalAddr() const noexcept
+  { return local_addr_; }
+
+  InetAddr const& GetPeerAddr() const noexcept
+  { return peer_addr_; }
+private:
+  InetAddr const local_addr_;
+  InetAddr const peer_addr_;
+};
+
+// Default connection callback.
+// It is called when connection established and closed.
+// Log message abont peer and local simply(trace level only)
+inline void DefaultConnectionCallback(TcpConnectionPtr const& conn) {
+  LOG_TRACE_KANON << conn->GetLocalAddr().ToIpPort() << "->"
+    << conn->GetPeerAddr().ToIpPort() << " "
+    << (conn->IsConnected() ? "UP" : "DOWN");
+}
+
+} // namespace kanon
+
+#endif // KANON_NET_TCPCONNECTION_H
