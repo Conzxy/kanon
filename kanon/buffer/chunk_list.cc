ab8e098cfd9363669600daeb58dc35e95696e213
Conzxy <1967933844@qq.com>
2022-06-11 16:16:46 +0800

    update: segmented continuous buffer

---
+++ kanon/buffer/chunk_list.cc
@@ -0,0 +1,200 @@
+#include "chunk_list.h"
+
+#include <sys/uio.h>
+#include <vector>
+#include <limits.h>
+#include <inttypes.h>
+
+#include "kanon/net/sock_api.h"
+#include "kanon/algo/fixed_vector.h"
+
+#ifdef IOV_MAX
+  static constexpr unsigned IOVEC_MAX = IOV_MAX;
+#else
+  static constexpr unsigned IOVEC_MAX = 1024;
+#endif
+
+// #define CHUNK_LIST_DEBUG
+
+#ifdef CHUNK_LIST_DEBUG
+  #include <stdio.h>
+
+  #define DLOG(str, ...) \
+    ::printf(str, __VA_ARGS__)
+#else
+  #define DLOG(str, ...) 
+#endif
+
+static size_t RoundUpDivideIn2(size_t x, size_t y) noexcept {
+  return (x == 0) ? 0 : ((x - 1) / y + 1);
+}
+
+namespace kanon {
+
+void ChunkList::Append(void const *data, size_t len) {
+  auto buf = reinterpret_cast<char const*>(data);
+
+  // If buffers_ is not empty, try fill the the last chunk
+  if (!buffers_.empty()) {
+    DLOG("%s", "===== append to last chunk =====\n");
+    auto last_block = buffers_.before_end();
+    auto last_block_wsize = last_block->GetWritableSize();
+
+    DLOG("Last chunk writable size = %zu\n", last_block_wsize);
+    if (last_block_wsize > 0) {
+      if (last_block_wsize >= len) {
+        last_block->Append(buf, len);
+        assert(last_block->GetWritableSize() == last_block_wsize-len);
+        DLOG("len = %zu < writable size of last chunk\n", len);
+        return;
+      } else {
+        last_block->Append(buf, last_block_wsize);
+        len -= last_block_wsize;
+        buf += last_block_wsize;
+      }
+    }
+  }
+  
+  ListType::Iterator free_buffer;
+
+  DLOG("%s", "===== Appent to the back of buffers_ =====\n");
+  while (len > 0) {
+    if (!free_buffers_.empty()) {
+      DLOG("%s", "===== Reuse the free chunk =====\n");
+      free_buffer = free_buffers_.extract_front();
+      buffers_.push_back(free_buffer);
+    } else {
+      DLOG("%s", "===== New chunk =====\n");
+      buffers_.push_back(buffers_.create_node_size(CHUNK_SIZE));
+      free_buffer = buffers_.before_end();
+    }
+
+    if (len >= CHUNK_SIZE) {
+      free_buffer->Append(buf, CHUNK_SIZE);
+      len -= CHUNK_SIZE;
+      buf += CHUNK_SIZE;
+    } else {
+      free_buffer->Append(buf, len);
+      return; 
+    }
+  }  
+}
+
+void ChunkList::AdvanceRead(size_t len) {
+  ListType::Iterator first_block;
+
+  while (!IsEmpty()) {
+    first_block = buffers_.begin();
+    auto const rsize = first_block->GetReadableSize();
+    DLOG("readable size = %zu\n", rsize);
+    // To the size() == 1, we also put it to free_buffers_
+    // then we can append content in CHUNK_SIZE at most
+    // since there is only one avaliable chunk to use
+    // reuse also can avoid to call ::malloc()
+    if (len >= first_block->GetReadableSize()) {
+      if (!PutToFreeChunk()) {
+        buffers_.drop_node(buffers_.extract_front_node());
+      }
+
+      len -= rsize;
+    } else {
+      first_block->AdvanceRead(len);
+      return;
+    }
+  }
+}
+
+auto ChunkList::GetReadableSize() const noexcept -> SizeType {
+  if (IsEmpty()) {
+    return 0;
+  } else {
+    auto first_chunk = buffers_.begin();
+    if (buffers_.size() == 1) {
+      return first_chunk->GetReadableSize();
+    } else if (buffers_.size() == 2) {
+      return first_chunk->GetReadableSize() + first_chunk.next()->GetReadableSize();
+    }
+
+    auto last_chunk = buffers_.before_end();
+    return first_chunk->GetMaxSize() != 4096 ? 
+      first_chunk->GetReadableSize()+first_chunk.next()->GetReadableSize()+(GetChunkSize()-3)*CHUNK_SIZE + last_chunk->GetReadableSize():
+      first_chunk->GetReadableSize()+(GetChunkSize()-2)*CHUNK_SIZE + last_chunk->GetReadableSize();
+  }
+}
+
+ChunkList::SizeType ChunkList::WriteFd(int fd, int& saved_errno) noexcept {
+  if (GetChunkSize() == 1) {
+    DLOG("%s", "Chunk size = 1, call ::write() directly\n");
+    auto first_chunk = GetFirstChunk();
+    return sock::Write(fd, GetFirstChunk()->GetReadBegin(), first_chunk->GetReadableSize());
+  }
+
+  FixedVector<struct iovec> iovecs(GetChunkSize());
+  auto first_chunk = buffers_.begin();
+  auto cnt = RoundUpDivideIn2(GetChunkSize(), IOVEC_MAX);
+
+  int ret = 0;
+
+  DLOG("%s\n", "===== Write iovecs =====");
+  while (cnt--) {
+    for (auto& x : iovecs) {
+      DLOG("readbegin = %p, readablesize = %zu\n", first_chunk->GetReadBegin(), first_chunk->GetReadableSize());
+      x.iov_base = first_chunk->GetReadBegin();
+      x.iov_len = first_chunk->GetReadableSize();
+      ++first_chunk;
+    }
+
+    auto n = ::writev(fd, iovecs.data(), iovecs.size());
+
+    if (n >= 0) {
+      ret += n;
+    } else {
+      saved_errno = errno;
+      break;
+    }
+  }
+
+  return ret;
+}
+
+bool ChunkList::PutToFreeChunk() noexcept {
+  if (buffers_.front().GetMaxSize() != CHUNK_SIZE) {
+    return false;
+  }
+
+  DLOG("%s\n", "Put a new free chunk");
+  auto free_buf = buffers_.extract_front();
+  free_buf->Reset();
+  free_buffers_.push_front(free_buf);
+  return true;
+}
+
+void ChunkList::Shrink(size_t chunk_size) {
+  if (chunk_size >= free_buffers_.size()) { return; }
+
+  auto end = free_buffers_.begin();
+  std::advance(end, free_buffers_.size()-chunk_size);
+
+  free_buffers_.erase_after(free_buffers_.cbefore_begin(), end);
+  assert(free_buffers_.size() == chunk_size);
+}
+
+void ChunkList::ReserveFreeChunk(size_t chunk_size) {
+  if (chunk_size <= free_buffers_.size()) { return; }
+
+  size_t diff = chunk_size - free_buffers_.size();
+
+  for (size_t i = 0; i < diff; ++i) {
+    free_buffers_.emplace_front();
+  }
+}
+
+auto ChunkList::GetFreeChunk() noexcept -> ListType::Iterator {
+  if (!free_buffers_.empty()) {
+    return free_buffers_.extract_front();
+  } else {
+    return free_buffers_.end();
+  }
+}
+
+} // namespace kanon
---
+++ kanon/buffer/chunk_list.h
@@ -0,0 +1,209 @@
+#ifndef KANON_NET_CHUNK_LIST_H
+#define KANON_NET_CHUNK_LIST_H
+
+#include <array>
+
+#include "kanon/string/string_view.h"
+#include "kanon/util/noncopyable.h"
+#include "kanon/util/mem.h"
+#include "kanon/algo/forward_list.h"
+#include "kanon/net/sock_api.h"
+
+namespace kanon {
+
+/**
+ * \brief variable-length buffer
+ * Because output buffer need prepend length header, allocate 
+ * fixed chunk is a waste.
+ * There is no field of char[] or char*, use the padding as buffer
+ * since node is not required to be continuous.
+ * There are two advantages:
+ * 1) Use char*, the locality is wrong than char[] or padding space and occupy space of pointer size
+ * 2) Use char[], must use fixed-size
+ */
+class Chunk {
+public:
+  explicit Chunk(size_t sz=4096)
+    : read_index_(0)
+    , write_index_(0)
+    , max_size_(sz)
+  { 
+  }
+
+  ~Chunk() = default;
+
+  void AdvanceRead(size_t len) noexcept {
+    assert(read_index_ <= write_index_);
+    read_index_ += len;
+    assert(read_index_ <= write_index_);
+  }
+
+  void AdvanceReadAll() noexcept {
+    AdvanceRead(GetReadableSize());
+  }
+
+  void Append(void const* data, size_t len) noexcept {
+    assert(len <= GetWritableSize());
+    ::memcpy(GetBuf()+write_index_, data, len);
+    write_index_ += len;
+  }
+
+  StringView ToStringView() noexcept {
+    return StringView(GetBuf()+read_index_, write_index_-read_index_);
+  }
+
+  char* GetReadBegin() noexcept { return GetBuf() + read_index_; }
+  char* GetWriteBegin() noexcept { return GetBuf() + write_index_; }
+  char const* GetReadBegin() const noexcept { return GetBuf() + read_index_; }
+  char const* GetWriteBegin() const noexcept { return GetBuf() + write_index_; }
+  size_t GetReadableSize() const noexcept { return write_index_ - read_index_; }
+  size_t GetWritableSize() const noexcept { return max_size_ - write_index_; }
+  size_t GetMaxSize() const noexcept { return max_size_; }
+
+  void Reset() noexcept {
+    read_index_ = write_index_ = 0;
+  }
+
+private:
+  // The buffer in the start position of the padding
+  char* GetBuf() noexcept { return reinterpret_cast<char*>(this) + sizeof(Chunk); }
+  char const* GetBuf() const noexcept { return reinterpret_cast<const char*>(this) + sizeof(Chunk); }
+
+  size_t read_index_;
+  size_t write_index_;
+  size_t max_size_;
+
+  // padding buffer
+};
+
+/**
+ * \brief Represents a fixed-size block linked-list
+ * 
+ * The node is a variable-size buffer chunk.
+ * Use the chunked continuous, we can achieve zero-copy.
+ * The buffer region in chunk is not occupy any space of a node, 
+ * need create intrusive nodes and append them.
+ * Therefore, std::forward_list<> can't satisfy my requirements:
+ * 1) There is no size() and xxx_back() methods to construct queue
+ * 2) There is no APIs that can create instrusive nodes
+ * On the contrary, in the basic of std::forward_list<>,
+ * I implemete zstl::ForwardList<> to meet these requirements.
+ *
+ * \note 
+ *   Public class
+ *   copyable and moveable
+ */
+class ChunkList final {
+  static constexpr unsigned CHUNK_SIZE = 4096;
+  using ListType = zstl::ForwardList<Chunk>;
+
+public:
+  using SizeType = ListType::size_type;
+
+  // The ForwardList is an internel class
+  ChunkList() = default;
+  ~ChunkList() noexcept = default;
+
+  ChunkList(ChunkList const&) = default;
+  ChunkList& operator=(ChunkList const& other) = default;
+  ChunkList(ChunkList&& other) noexcept = default;
+  ChunkList& operator=(ChunkList&& other) noexcept = default;
+
+  void Append(void const* data, size_t len);
+  void Append(StringView data) { Append(data.data(), data.size()); }
+
+#define AppendInt_Macro(size) \
+  void Append##size(uint##size##_t i) { \
+    auto ni = sock::ToNetworkByteOrder##size(i); \
+    Append(&ni, sizeof ni); \
+  }
+
+  void Append8(uint8_t i) {
+    Append(&i, sizeof i);
+  }
+
+  AppendInt_Macro(16)
+  AppendInt_Macro(32)
+  AppendInt_Macro(64)
+
+/**
+ * Prepend a new chunk 
+ */
+#define PrependInt_Macro(size) \
+  void Prepend##size(uint##size##_t i) noexcept { \
+    buffers_.push_front(buffers_.create_node_size(sizeof(i), sizeof(i))); \
+    assert(buffers_.front().GetMaxSize() == sizeof(i)); \
+    auto ni = sock::ToNetworkByteOrder##size(i); \
+    assert(sock::ToHostByteOrder##size(ni) == i); \
+    buffers_.front().Append(&ni, sizeof ni); \
+    assert(buffers_.front().GetReadableSize() == sizeof(i)); \
+  }
+
+  void Prepend8(uint8_t i) {
+    buffers_.push_front(buffers_.create_node_size(sizeof(i), sizeof(i)));
+    assert(buffers_.front().GetMaxSize() == sizeof(i));
+    buffers_.front().Append(&i, sizeof i);
+  }
+
+  PrependInt_Macro(16)
+  PrependInt_Macro(32)
+  PrependInt_Macro(64)
+
+#define ReadInt_Macro(size) \
+  uint##size##_t Read##size() noexcept { \
+    assert(buffers_.front().GetReadableSize() == sizeof(uint##size##_t)); \
+    assert(buffers_.front().GetMaxSize() == sizeof(uint##size##_t)); \
+    return sock::ToHostByteOrder##size(*reinterpret_cast<uint##size##_t*>(buffers_.front().GetReadBegin())); \
+  }
+
+  uint8_t Read8() noexcept {
+    assert(buffers_.front().GetReadableSize() == sizeof(uint8_t));
+    return *reinterpret_cast<uint8_t*>(buffers_.front().GetReadBegin());
+  } 
+
+  ReadInt_Macro(16)
+  ReadInt_Macro(32)
+  ReadInt_Macro(64)
+
+  void AdvanceRead(size_t len);
+  void AdvanceReadAll() { AdvanceRead(GetReadableSize()); }
+  void Shrink(size_t chunk_size);
+  void ReserveFreeChunk(size_t chunk_size);
+
+  SizeType GetChunkSize() const noexcept { return buffers_.size(); }
+  SizeType GetFreeChunkSize() const noexcept { return free_buffers_.size(); }
+  SizeType GetReadableSize() const noexcept;
+
+  bool IsEmpty() const noexcept { return buffers_.empty(); }
+  bool HasReadable() const noexcept { return !IsEmpty(); }
+
+  Chunk* GetFirstChunk() noexcept { return &buffers_.front(); }
+  Chunk const* GetFirstChunk() const noexcept { return &buffers_.front(); }
+  Chunk* GetLastChunk() noexcept { return &buffers_.back(); }
+  Chunk const* GetLastChunk() const noexcept { return &buffers_.back(); }
+  SizeType WriteFd(int fd, int& saved_errno) noexcept;
+  SizeType WriteFd(int fd) noexcept { int saved_errno; return WriteFd(fd, saved_errno); }
+  void swap(ChunkList& other) noexcept {
+    buffers_.swap(other.buffers_);
+    free_buffers_.swap(other.free_buffers_);
+  }
+
+  // void SetFreeMaxSize(size_t max_size) noexcept { free_max_size_ = max_size; }
+  // size_t GetFreeMaxSize() const noexcept { return free_max_size_; }
+  static SizeType GetSingleChunkSize() noexcept { return CHUNK_SIZE; }
+
+private:
+  bool PutToFreeChunk() noexcept;
+  ListType::Iterator GetFreeChunk() noexcept;
+
+  ListType buffers_;
+  ListType free_buffers_;
+};
+
+inline void swap(ChunkList& lhs, ChunkList& rhs) noexcept {
+  lhs.swap(rhs);
+}
+
+} // namespace kanon
+
+#endif // KANON_NET_CHUNK_LIST_H
