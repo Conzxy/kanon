bdccabede241882e9df8f01fd9fdffd77cf8f509
Conzxy <Conzxy@1967933844@qq.com>
2022-06-11 16:30:11 +0800

    update: add ChunkList

---
+++ kanon/net/user_common.h
@@ -0,0 +1,21 @@
+#ifndef KANON_NET_USER_COMMON_H
+#define KANON_NET_USER_COMMON_H
+
+#include "kanon/net/event_loop.h"
+#include "kanon/net/event_loop_thread.h"
+#include "kanon/net/connection/tcp_connection.h"
+#include "kanon/buffer/chunk_list.h"
+#include "kanon/net/buffer.h"
+#include "kanon/net/inet_addr.h"
+
+using kanon::EventLoop;
+using kanon::EventLoopThread;
+using kanon::TcpConnectionPtr;
+using kanon::Buffer;
+using kanon::InetAddr;
+using kanon::TimeStamp;
+using kanon::StringView;
+using kanon::OutputBuffer;
+using kanon::ChunkList;
+
+#endif // KANON_NET_USRE_COMMON_H
