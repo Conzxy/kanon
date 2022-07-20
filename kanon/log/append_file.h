05c7f8a861982126639d965529f31b57243f05cb
Conzxy <1967933844@qq.com>
2022-07-21 01:28:50 +0800

    update: accept void const*

--- kanon/log/append_file.h
+++ kanon/log/append_file.h
@@ -16,7 +16,7 @@ public:
   explicit AppendFile(StringArg filename);
   ~AppendFile() noexcept;
   
-  void Append(char const* data, size_t num) noexcept;
+  void Append(void const* data, size_t num) noexcept { _Append((char const*)data, num); }
 
   void Flush() noexcept
   { ::fflush_unlocked(fp_); }
@@ -28,6 +28,7 @@ private:
   size_t write(char const* data, size_t num) noexcept
   { return ::fwrite(data, 1, num, fp_); }
 
+  void _Append(char const* data, size_t num) noexcept;
 protected:
   char buf_[64 * 1024];
   // indicator of roll file
