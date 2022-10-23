bbf5811006c8ff732262f215c4ce362d8900ce0c
Conzxy <1967933844@qq.com>
2022-10-23 23:45:31 +0800

    leak_detector facility(libgc)

---
+++ test/leak_detector.h
@@ -0,0 +1,12 @@
+#ifndef CONZXY_LEAK_DETECTOR_H__
+#define CONZXY_LEAK_DETECTOR_H__
+
+#define GC_DEBUG
+#include "gc.h"
+#define malloc(n) GC_MALLOC(n)
+#define calloc(m,n) GC_MALLOC((m)*(n))
+#define free(p) GC_FREE(p)
+#define realloc(p,n) GC_REALLOC((p),(n))
+#define CHECK_LEAKS() GC_gcollect()
+
+#endif 
