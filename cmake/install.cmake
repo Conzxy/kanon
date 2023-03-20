7236221f4fa01f826969e486216bf0a239a71e3b
Conzxy <1967933844@qq.com>
2023-03-20 22:19:38 +0800

    Rename and comments

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -2,28 +2,36 @@ function (conzxy_install)
   cmake_parse_arguments(PARSE_ARGV 0 CONZXY "" "PROJECT;NAMESPACE" "TARGETS")
 
   include(GNUInstallDirs)
-  set(project_config_in "${CMAKE_CURRENT_LIST_DIR}/cmake/${CONZXY_PROJECT}Config.cmake.in")
-  set(project_config_out "${CMAKE_CURRENT_BINARY}/${CONZXY_PROJECT}Config.cmake")
-  set(config_targets_file "${CONZXY_PROJECT}ConfigTargets.cmake")
-  set(version_config_file "${CMAKE_CURRENT_BINARY}/${CONZXY_PROJECT}ConfigVersion.cmake")
+  set(project_config_in_file "${CMAKE_CURRENT_LIST_DIR}/cmake/${CONZXY_PROJECT}Config.cmake.in")
+  set(project_config_out_file "${CMAKE_CURRENT_BINARY}/${CONZXY_PROJECT}Config.cmake")
+  set(config_targets_filename "${CONZXY_PROJECT}ConfigTargets.cmake")
+  set(config_version_file "${CMAKE_CURRENT_BINARY}/${CONZXY_PROJECT}ConfigVersion.cmake")
   set(export_dest_dir "${CMAKE_INSTALL_LIBDIR}/cmake/${CONZXY_PROJECT}")
-
+    
+  # Specify rules about targets
   install(
     TARGETS ${CONZXY_TARGETS}
     EXPORT ${CONZXY_PROJECT}
-    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+    ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+    RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
   )
-
+    
   export(
-      TARGETS ${CONZXY_TARGETS}
+      EXPORT ${CONZXY_PROJECT}
       NAMESPACE ${CONZXY_NAMESPACE}::
-      FILE "${CMAKE_CURRENT_BINARY}/${config_targets_file}")
-  install(EXPORT ${CONZXY_PROJECT} DESTINATION "${export_dest_dir}" NAMESPACE ${CONZXY_NAMESPACE}:: FILE ${config_targets_file})
-
+      FILE "${CMAKE_CURRENT_BINARY_DIR}/${config_targets_filename}")
+  # Install ConfigTargets.cmake
+  install(EXPORT ${CONZXY_PROJECT} 
+      DESTINATION "${export_dest_dir}"
+      NAMESPACE ${CONZXY_NAMESPACE}::
+      FILE ${config_targets_filename})
+    
+  # Install Config.cmake and ConfigVersion.cmake
   include(CMakePackageConfigHelpers)
-  configure_package_config_file("${project_config_in}" "${project_config_out}" INSTALL_DESTINATION "${export_dest_dir}")
-  write_basic_package_version_file("${version_config_file}" COMPATIBILITY SameMajorVersion)
-  install(FILES "${project_config_out}" "${version_config_file}" DESTINATION "${export_dest_dir}")
+  # (<in> <out>)
+  configure_package_config_file("${project_config_in_file}" "${project_config_out_file}" 
+      INSTALL_DESTINATION "${export_dest_dir}")
+  write_basic_package_version_file("${config_version_file}" COMPATIBILITY SameMajorVersion)
+  install(FILES "${project_config_out_file}" "${config_version_file}" DESTINATION "${export_dest_dir}")
 endfunction ()
