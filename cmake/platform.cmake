877118251eadccce4b6257cfb51f52fcb1692f17
Conzxy <1967933844@qq.com>
2023-03-24 23:08:39 +0800

    platfrom setting(TODO)

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,12 +1,13 @@
 # In other platform(eg. windows), generating base library only.
 if (UNIX AND NOT APPLE)
   message(STATUS "Kanon on unix/linux/solaris/... platform")
-  set(KANON_ON_UNIX 1)
+  set(KANON_ON_UNIX ON)
 else ()
-  set(AKNON_ON_UNIX 0)
+  set(AKNON_ON_UNIX OFF)
 endif ()
 
 if (WIN32)
   message(STATUS "Kanon on windows")
+  set(KANON_ON_WIN ON)
 endif ()
 
