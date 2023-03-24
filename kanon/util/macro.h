410fa32367eeeb32ff467f39c88d9e9c6fbd3c84
Conzxy <1967933844@qq.com>
2023-03-24 23:10:34 +0800

    Compatible to windows

--- kanon/util/macro.h
+++ kanon/util/macro.h
@@ -1,9 +1,13 @@
 #ifndef KANON_UTIL_MACRO_H
 #define KANON_UTIL_MACRO_H
 
+#define _XKEYCHECK_H
+
 #define KANON_UNUSED(var) (void)var
 #define KANON_RETHROW throw
 
+/* In windows, you add /Zc:__cplusplus option to MSVC,
+   the __cplusplus >= 201402L at least(i.e. c++14) */
 #if __cplusplus >= 201103L
 #define CXX_STANDARD_11
 #endif
@@ -34,19 +38,26 @@
 #define KANON_CONSTEXPR inline
 #endif
 
+#if defined(__GNUC__) || defined(__clang__)
+#define KANON_ALWAYS_INLINE __attribute__((always_inline))
+#elif defined(_MSC_VER) /* && !defined(__clang__) */
+#define KANON_ALWAYS_INLINE __forceinline
+#else
+#define KANON_ALWAYS_INLINE
+#endif // !defined(__GUNC__) || defined(__clang__)
+
+#define KANON_INLINE inline KANON_ALWAYS_INLINE
+
 #define KANON_RETHROW throw
-#define KANON_FUNCTION_TRY try: 
-#define KANON_CATCH catch 
-#define KANON_CATCH_ALL catch (...) 
+#define KANON_FUNCTION_TRY try:
+#define KANON_CATCH catch
+#define KANON_CATCH_ALL catch (...)
 
-#define BZERO(dst, num) \
-  ::memset(dst, 0, num)
+#define BZERO(dst, num) ::memset(dst, 0, num)
 
-#define IMPORT_NAMESPACE(space) \
-  using namespace space
+#define IMPORT_NAMESPACE(space) using namespace space
 
-#define KANON_ASSERT(cond, msg) \
-  assert((cond) && (msg))
+#define KANON_ASSERT(cond, msg) assert((cond) && (msg))
 
 #ifdef __linux__
 #define KANON_ON_LINUX 1
@@ -58,6 +69,14 @@
 
 #if (defined(__unix__) || defined(__linux__)) && !defined(KANON_TEST_THREAD)
 #define KANON_ON_UNIX 1
+#endif
+
+#if defined(__GUNC__) || defined(__clang__)
+#define KANON_LIKELY(cond) KANON_BUILTIN_EXPECT(!!(cond), 1)
+#define KANON_UNLIKELY(cond) KANON_BUILTIN_EXPECT(!!(cond), 0)
+#else
+#define KANON_LIKELY(cond) (cond)
+#define KANON_UNLIKELY(cond) (cond)
 #endif
 
 #endif // KANON_UTIL_MACRO_H
