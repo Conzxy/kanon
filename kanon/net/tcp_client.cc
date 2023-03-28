3548fc5013a6f03a5ad71af60a8208826c1d4552
Conzxy <1967933844@qq.com>
2023-03-28 19:04:12 +0800

    MOD: client compatible

--- kanon/net/tcp_client.cc
+++ kanon/net/tcp_client.cc
@@ -1,3 +1,8 @@
+#include "kanon/util/platform_macro.h"
+#ifdef KANON_ON_WIN
+#include <winsock2.h>
+#endif
+
 #include "kanon/net/tcp_client.h"
 
 #include "kanon/net/event_loop.h"
@@ -6,52 +11,52 @@
 #include "kanon/net/connection/tcp_connection.h"
 #include "kanon/net/sock_api.h"
 #include "kanon/net/user_client.h"
+#include "kanon/net/channel.h"
 
 using namespace kanon;
 
-TcpClient::TcpClient(
-  EventLoop* loop,
-  InetAddr const& serv_addr,
-  std::string const& name)
-  : loop_{ loop }
-  // Use std::make_shared is ok here since no weak pointer
-  , connector_{ Connector::NewConnector(loop, serv_addr) }
-  , name_{ name }
-  , connection_callback_{ &DefaultConnectionCallback }
-  // , connect_{ true }
-  , retry_{ false }
-  , conn_{ nullptr }
-  , mutex_{ }
+TcpClient::TcpClient(EventLoop *loop, InetAddr const &serv_addr,
+                     std::string const &name)
+  : loop_{loop} // Use std::make_shared is ok here since no weak pointer
+  , connector_{Connector::NewConnector(loop, serv_addr)}
+  , name_{name}
+  , connection_callback_{&DefaultConnectionCallback} // , connect_{ true }
+  , retry_{false}
+  , conn_{nullptr}
+  , mutex_{}
 {
-  LOG_TRACE_KANON << "TcpClient-[" << name_ << "]" << " is constructed";
+  LOG_TRACE_KANON << "TcpClient-[" << name_ << "]"
+                  << " is constructed";
+}
 
-} 
-
-InetAddr const& TcpClient::GetServerAddr() const noexcept {
+InetAddr const &TcpClient::GetServerAddr() const noexcept
+{
   return connector_->GetServerAddr();
 }
 
-TcpClient::~TcpClient() noexcept {
-  LOG_TRACE_KANON << "TcpClient-[" << name_ << "]" << " is destructed";
+TcpClient::~TcpClient() noexcept
+{
+  LOG_TRACE_KANON << "TcpClient-[" << name_ << "]"
+                  << " is destructed";
 
   bool is_unique = false;
   TcpConnectionPtr conn;
   {
-    MutexGuard guard{ mutex_ };
+    MutexGuard guard{mutex_};
 
     is_unique = (conn_.use_count() == 1); // not atomic
 
     // To write or read conn_ is not thread-safe,
-    // we can copy it first, for the duplication, 
-    // it is thread-safe. 
+    // we can copy it first, for the duplication,
+    // it is thread-safe.
     // Because conn_ if is not released, conn also.
     // In the following operations, it is also alive
-    // since ref count is not 0(i.e. pinned). Otherwise, 
+    // since ref count is not 0(i.e. pinned). Otherwise,
     // conn is released. we can't access it.
     conn = conn_;
-  } 
-  
-  // Has established new connection 
+  }
+
+  // Has established new connection
   if (conn) {
     assert(conn->GetLoop() == loop_);
     // Should not use old close callback
@@ -63,30 +68,32 @@ TcpClient::~TcpClient() noexcept {
     // Disable the connector
     connector_->Stop();
   }
-  
 }
 
-void TcpClient::Connect() {
-  connector_->StartRun();
-}
+void TcpClient::Connect() { connector_->StartRun(); }
 
-void TcpClient::Disconnect() {
+void TcpClient::Disconnect()
+{
   retry_ = false;
   // FIXME use weak callback?
-  MutexGuard guard{ mutex_ };
+  MutexGuard guard{mutex_};
   if (conn_) {
     conn_->ShutdownWrite();
   }
 }
 
-void TcpClient::Stop() {
+void TcpClient::Stop()
+{
   retry_ = false;
   // in loop
   connector_->Stop();
 }
 
-void TcpClient::Init() {
-  connector_->SetNewConnectionCallback(std::bind(TcpClient::NewConnection, _1, GetServerAddr(), std::weak_ptr<TcpClient>(shared_from_this())));
+void TcpClient::Init()
+{
+  connector_->SetNewConnectionCallback(
+      std::bind(TcpClient::NewConnection, _1, GetServerAddr(),
+                std::weak_ptr<TcpClient>(shared_from_this())));
 }
 
 /*
@@ -95,45 +102,51 @@ void TcpClient::Init() {
  * TcpClient contains std::shared_ptr<Connector>
  * Connector contains std::shared_ptr<TcpClient>
  */
-void TcpClient::NewConnection(int sockfd, InetAddr const &serv_addr, std::weak_ptr<TcpClient> const &wcli) {
+void TcpClient::NewConnection(FdType sockfd, InetAddr const &serv_addr,
+                              std::weak_ptr<TcpClient> const &wcli)
+{
   /* Avoid reference cycle */
   auto cli = wcli.lock();
 
   if (!cli || cli->conn_) return;
-  
+
   LOG_TRACE << " New connection fd = " << sockfd;
-  auto new_conn = TcpConnection::NewTcpConnection(cli->loop_, cli->name_, sockfd, sock::GetLocalAddr(sockfd), serv_addr);
-  
+  auto new_conn = TcpConnection::NewTcpConnection(
+      cli->loop_, cli->name_, sockfd, sock::GetLocalAddr(sockfd), serv_addr);
+
+  LOG_TRACE << "New connection: " << new_conn.get();
   // Must copy callback instead of moving them
   new_conn->SetMessageCallback(cli->message_callback_);
   new_conn->SetWriteCompleteCallback(cli->write_complete_callback_);
   new_conn->SetConnectionCallback(cli->connection_callback_);
+#ifdef KANON_ON_WIN
+  new_conn->SetChannel(cli->connector_->channel());
+#endif
 
   // If we directly contruct and set new connection,
   // that is dangerous! we may be get a half-completed connection.
   {
-    MutexGuard guard{ cli->mutex_ };
+    MutexGuard guard{cli->mutex_};
     cli->conn_ = std::move(new_conn);
   }
 
   // RemoveConnection
-  cli->conn_->SetCloseCallback([cli](TcpConnectionPtr const& conn) {
+  cli->conn_->SetCloseCallback([cli](TcpConnectionPtr const &conn) {
     cli->loop_->AssertInThread();
     // passive close connection
     assert(conn == cli->conn_);
     assert(conn->GetLoop() == cli->loop_);
 
-
-    // Like TcpServer, we remove connection from TcpClient 
+    // Like TcpServer, we remove connection from TcpClient
     {
-      MutexGuard guard{ cli->mutex_ };
+      MutexGuard guard{cli->mutex_};
       cli->conn_.reset();
       LOG_INFO << conn->GetName() << " has removed";
-    } 
+    }
 
     // ! In event handling phase, don't remove channel(disable is allowed)
     // ! conn must be copied(\see TcoConnection::HandleClose())
-    cli->loop_->QueueToLoop([conn] () {
+    cli->loop_->QueueToLoop([conn]() {
       conn->ConnectionDestroyed();
     });
 
@@ -148,11 +161,13 @@ void TcpClient::NewConnection(int sockfd, InetAddr const &serv_addr, std::weak_p
   cli->conn_->ConnectionEstablished();
 }
 
-TcpClientPtr kanon::NewTcpClient(EventLoop *loop, InetAddr const &serv_addr, std::string const &name, bool compact) 
+TcpClientPtr kanon::NewTcpClient(EventLoop *loop, InetAddr const &serv_addr,
+                                 std::string const &name, bool compact)
 {
-  auto ret = compact?
-             MakeSharedFromProtected<TcpClient>(loop, serv_addr, name) :
-             std::shared_ptr<TcpClient>(new TcpClient(loop, serv_addr, name));
+  auto ret =
+      compact
+          ? MakeSharedFromProtected<TcpClient>(loop, serv_addr, name)
+          : std::shared_ptr<TcpClient>(new TcpClient(loop, serv_addr, name));
   ret->Init();
   return ret;
 }
--- kanon/net/tcp_client.h
+++ kanon/net/tcp_client.h
@@ -7,6 +7,7 @@
 #include "kanon/util/macro.h"
 #include "kanon/util/noncopyable.h"
 #include "kanon/util/ptr.h"
+#include "kanon/net/type.h"
 
 #include "kanon/thread/mutex_lock.h"
 
@@ -32,8 +33,9 @@ using TcpClientPtr = std::shared_ptr<TcpClient>;
  * get the connection to send message.
  * \note Public class
  */
-class TcpClient : noncopyable 
-                , public std::enable_shared_from_this<TcpClient> {
+class TcpClient
+  : noncopyable
+  , public std::enable_shared_from_this<TcpClient> {
 
  protected:
   /**
@@ -41,22 +43,27 @@ class TcpClient : noncopyable
    * \param serv_addr Address of server that you want to connect
    * \param name Can be empty
    */
-  TcpClient(EventLoop* loop, 
-            InetAddr const& serv_addr,
-            std::string const& name = {});
+  TcpClient(EventLoop *loop, InetAddr const &serv_addr,
+            std::string const &name = {});
 
  public:
   ~TcpClient() noexcept;
 
   void SetConnectionCallback(ConnectionCallback cb) noexcept
-  { connection_callback_ = std::move(cb); }
+  {
+    connection_callback_ = std::move(cb);
+  }
 
   void SetMessageCallback(MessageCallback cb) noexcept
-  { message_callback_ = std::move(cb); }
+  {
+    message_callback_ = std::move(cb);
+  }
 
   void SetWriteCompleteCallback(WriteCompleteCallback cb) noexcept
-  { write_complete_callback_ = std::move(cb); }
-  
+  {
+    write_complete_callback_ = std::move(cb);
+  }
+
   //! Active connect
   void Connect();
   //! Active close
@@ -68,53 +75,55 @@ class TcpClient : noncopyable
    */
   void Stop();
 
-  InetAddr const& GetServerAddr() const noexcept;
+  InetAddr const &GetServerAddr() const noexcept;
 
   /**
    * Enable the client retry connect when client
    * is closed by peer(But if you call disconnect
    * early, this is useless)
-   */ 
-  void EnableRetry() noexcept
-  { retry_ = true; }
+   */
+  void EnableRetry() noexcept { retry_ = true; }
 
   //! \name getter
   //!@{
 
   /**
    * \brief Get the connection
-   * 
+   *
    * I don't implement this through condition variable
-   * to make caller thread sleeping when connection is not 
+   * to make caller thread sleeping when connection is not
    * established since it will block the caller thread.
    * To GUI program, this is might not good behavior.
    *
-   * If you want to block caller thread until connection 
+   * If you want to block caller thread until connection
    * is established in the other thread, you can notify
    * caller thread in the ConnectionCallback
    * \return
    *   This only return nullptr or established conection.
    * \note Thread-safe
    */
-  TcpConnectionPtr GetConnection() const noexcept {
-    MutexGuard guard{ mutex_ };
+  TcpConnectionPtr GetConnection() const noexcept
+  {
+    MutexGuard guard{mutex_};
     return conn_;
   }
 
-  EventLoop* GetLoop() noexcept { return loop_; }
+  EventLoop *GetLoop() noexcept { return loop_; }
 
   //!@}
  private:
-  friend TcpClientPtr NewTcpClient(EventLoop*, InetAddr const&, std::string const&, bool);
+  friend TcpClientPtr NewTcpClient(EventLoop *, InetAddr const &,
+                                   std::string const &, bool);
 
   /* Register the callback
    * since can't do it in the ctor */
   void Init();
-  
-  /* Callback of Connector::NewConnection */
-  static void NewConnection(int sockfd, InetAddr const &serv_addr, std::weak_ptr<TcpClient> const &cli);
 
-  EventLoop* loop_;
+  /* Callback of Connector::NewConnection */
+  static void NewConnection(FdType sockfd, InetAddr const &serv_addr,
+                            std::weak_ptr<TcpClient> const &cli);
+
+  EventLoop *loop_;
   std::shared_ptr<Connector> connector_;
 
   const std::string name_;
@@ -127,8 +136,8 @@ class TcpClient : noncopyable
   /*
    * For passive close connection.
    * If peer close connection, then restart connecting.
-   * 
-   * If connect_ is false, indicates the user 
+   *
+   * If connect_ is false, indicates the user
    * close peer actively or connector is disabled.
    * In such case, retry don't work.
    */
@@ -142,10 +151,8 @@ class TcpClient : noncopyable
  * \brief Create a tcp client in correct approach
  * \param compact Call std::make_shared if true, otherwise use `new` operator
  */
-TcpClientPtr NewTcpClient(EventLoop *loop,
-                          InetAddr const &serv_add,
-                          std::string const &name = {},
-                          bool compact=true);
+TcpClientPtr NewTcpClient(EventLoop *loop, InetAddr const &serv_add,
+                          std::string const &name = {}, bool compact = true);
 
 //!@}
 } // namespace kanon
