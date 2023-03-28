a7c2cb194c273ab5a272e50d427131c1a1d37cba
Conzxy <1967933844@qq.com>
2023-03-29 00:52:36 +0800

    FIX: Add() operator is + instead of -

--- kanon/thread/count_down_latch.h
+++ kanon/thread/count_down_latch.h
@@ -12,52 +12,48 @@
 // 1) main thread wait other thread complete their task
 namespace kanon {
 
-class CountDownLatch : noncopyable
-{
-public:
+class CountDownLatch : noncopyable {
+ public:
   CountDownLatch()
     : CountDownLatch(0)
-  { }
+  {
+  }
 
   explicit CountDownLatch(int count)
-    : count_(count), mutex_(), cond_(mutex_)
-  { }
-
-  void Reset(int count) noexcept
-  { count_ = count; }
-
-  void Add(int count) noexcept {
-    count_ -= count;
+    : count_(count)
+    , mutex_()
+    , cond_(mutex_)
+  {
   }
 
-  void Minus(int count) noexcept {
-    count_ -= count;
-  }
+  void Reset(int count) noexcept { count_ = count; }
+
+  void Add(int count) noexcept { count_ += count; }
+
+  void Minus(int count) noexcept { count_ -= count; }
 
   void Incr() noexcept { Add(1); }
   void Decr() noexcept { Minus(1); }
 
-  int GetCount() const noexcept
-  { return count_; }
+  int GetCount() const noexcept { return count_; }
 
   void Wait()
   {
     MutexGuard guard(mutex_);
-    if(count_ > 0)
-      cond_.Wait();
+    if (count_ > 0) cond_.Wait();
   }
 
   void Countdown() noexcept
   {
     MutexGuard guard(mutex_);
-    if(--count_ == 0)
-      cond_.NotifyAll();
+    if (--count_ == 0) cond_.NotifyAll();
   }
-private:
+
+ private:
   int count_;
   MutexLock mutex_;
   Condition cond_;
-}; 
+};
 
 } // namespace kanon
 
