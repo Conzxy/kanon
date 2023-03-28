1c04b25b4c759128f9ce17bd641dcf6a1bc01c94
Conzxy <1967933844@qq.com>
2023-03-28 19:05:23 +0800

    MOD: get Win thread id

--- kanon/thread/thread.cc
+++ kanon/thread/thread.cc
@@ -1,16 +1,18 @@
 #include "kanon/thread/thread.h"
 
+#ifdef KANON_ON_UNIX
+#include <sys/prctl.h>
+#elif defined(KANON_ON_WIN)
+#include <windows.h>
+#include <processthreadsapi.h>
+#endif
+
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
 
-#ifdef KANON_ON_UNIX
-#include <sys/prctl.h>
-#else
 #include <system_error>
-#endif
-
-#include "kanon/thread/atomic.h"
+// #include "kanon/thread/atomic.h"
 #include "kanon/thread/atomic_counter.h"
 #include "kanon/thread/current_thread.h"
 
@@ -20,28 +22,29 @@
 
 using namespace std;
 
-namespace kanon{
+namespace kanon {
 
 AtomicCounter32 Thread::numCreated_{};
 
-Thread::Thread(std::string const& name)
+Thread::Thread(std::string const &name)
   : Thread(Threadfunc(), name)
 {
 }
 
-Thread::Thread(Threadfunc func, string const& name)
-  : func_{std::move(func)},
-    is_started_{false},
-    is_joined_{false},
-    pthreadId_{0},
-    name_{name}
+Thread::Thread(Threadfunc func, string const &name)
+  : func_{std::move(func)}
+  , is_started_{false}
+  , is_joined_{false}
+  , pthreadId_{0}
+  , name_{name}
 {
   numCreated_.Add(1);
   SetDefaultName();
 }
 
-Thread::~Thread(){
-  if(is_started_ && !is_joined_) {
+Thread::~Thread()
+{
+  if (is_started_ && !is_joined_) {
 #ifdef KANON_ON_UNIX
     ::pthread_detach(pthreadId_);
 #else
@@ -50,15 +53,16 @@ Thread::~Thread(){
   }
 }
 
-void Thread::StartRun(){
+void Thread::StartRun()
+{
   assert(!is_started_);
   is_started_ = false;
-  
+
   // auto data = new detail::ThreadData{ func_, name_ };
 #ifdef KANON_ON_UNIX
   int err = 0;
   err = pthread_create(&pthreadId_, NULL, &detail::Run, this);
-  if(err != 0){
+  if (err != 0) {
     char buf[128];
     auto ret = ::strerror_r(errno, buf, sizeof buf);
     KANON_UNUSED(ret);
@@ -71,18 +75,25 @@ void Thread::StartRun(){
 #else
   try {
     std::thread thr(&detail::Run, this);
+#ifdef KANON_ON_UNIX
     pthreadId_ = thr.native_handle();
+#else
+    pthreadId_ = GetThreadId(thr.native_handle());
+#endif
     thr_ = std::move(thr);
     is_started_ = true;
-  } catch (std::system_error const &ex) {
-    fprintf(stderr, "Failed to create thread: %s!\n", ex.what())
+  }
+  catch (std::system_error const &ex) {
+    fprintf(stderr, "Failed to create thread: %s!\n", ex.what());
     exit(1);
-  } catch (...) {
+  }
+  catch (...) {
     fprintf(stderr, "Failed to create thread for unknown reason!");
     exit(1);
   }
 #endif
-  // if ((err = pthread_create(&pthreadId_, NULL, &detail::startRun, data)) != 0) {
+  // if ((err = pthread_create(&pthreadId_, NULL, &detail::startRun, data)) !=
+  // 0) {
 }
 
 void Thread::StartRun(Threadfunc cb)
@@ -91,12 +102,13 @@ void Thread::StartRun(Threadfunc cb)
   StartRun();
 }
 
-void Thread::Join(){
+void Thread::Join()
+{
   assert(is_started_);
   assert(!is_joined_);
 
   is_joined_ = true;
-  
+
 #ifdef KANON_ON_UNIX
   pthread_join(pthreadId_, NULL);
 #else
@@ -104,42 +116,50 @@ void Thread::Join(){
 #endif
 }
 
-void Thread::SetDefaultName(){
-  if(!name_.empty()) return ;
-  
+void Thread::SetDefaultName()
+{
+  if (!name_.empty()) return;
+
   char buf[64];
   BZERO(buf, sizeof buf);
-  
+
   snprintf(buf, sizeof buf, "KanonThread%d", numCreated_.GetValue());
   name_ = buf;
 }
 
 namespace detail {
 
-void* Run(void* arg) {
+void *Run(void *arg)
+{
   KANON_UNUSED(CurrentThread::tid());
-  auto thread = reinterpret_cast<Thread*>(arg);
-  CurrentThread::t_name = thread->name_.empty() ? "KanonThread" : thread->name_.c_str();
+  auto thread = reinterpret_cast<Thread *>(arg);
+  CurrentThread::t_name =
+      thread->name_.empty() ? "KanonThread" : thread->name_.c_str();
 
   // process control
   // set calling thread name
-  // \note: the second argument just up to 16 bytes(including terminating null byte)
+  // \note: the second argument just up to 16 bytes(including terminating null
+  // byte)
 #ifdef KANON_ON_UNIX
   ::prctl(PR_SET_NAME, CurrentThread::t_name);
 #else
-  // TODO set thread name in WINDOWS 
+  // TODO set thread name in WINDOWS
 #endif
-  
+
   try {
     thread->func_();
     CurrentThread::t_name = "finished";
-  } catch(std::exception const& ex) {
-    ::fprintf(stderr, "std::exception is caught in Thread %s\n", thread->name_.c_str());
+  }
+  catch (std::exception const &ex) {
+    ::fprintf(stderr, "std::exception is caught in Thread %s\n",
+              thread->name_.c_str());
     ::fprintf(stderr, "Reason: %s", ex.what());
     ::fflush(stderr);
     ::abort(); // don't throw or return just terminate
-  } catch(...) {
-    ::fprintf(stderr, "Unknown exception is caught in Thread %s\n", thread->name_.c_str());
+  }
+  catch (...) {
+    ::fprintf(stderr, "Unknown exception is caught in Thread %s\n",
+              thread->name_.c_str());
     ::abort();
   }
 
@@ -162,7 +182,7 @@ void* Run(void* arg) {
 // };
 
 // ThreadData::ThreadData(Threadfunc func, string const& name)
-//   : func_{std::move(func)}, 
+//   : func_{std::move(func)},
 //     name_{name}
 // {}
 
@@ -172,9 +192,10 @@ void* Run(void* arg) {
 
 //   // process control
 //   // set calling thread name
-//   // \note: the second argument just up to 16 bytes(including terminating null byte)
+//   // \note: the second argument just up to 16 bytes(including terminating
+//   null byte)
 //   ::prctl(PR_SET_NAME, CurrentThread::t_name);
-  
+
 //   try {
 //     func_();
 //     CurrentThread::t_name = "finished";
