5c5a49ad9d8398a8b0b3ce9564b9e37709d9c269
Conzxy <1967933844@qq.com>
2023-03-28 18:32:49 +0800

    MOD: Win replace setbuffer() with setvbuf()

--- kanon/log/append_file.cc
+++ kanon/log/append_file.cc
@@ -16,56 +16,59 @@ namespace kanon {
 AppendFile::AppendFile(StringArg filename)
   : writtenBytes_(0)
 {
-  while (! (fp_ = ::fopen(filename.data(), "a") ) ) {
+  while (!(fp_ = ::fopen(filename.data(), "a"))) {
     if (errno != ENOENT) {
-      ::fprintf(stderr, "Failed to open file: %s\n", filename.data());  
+      ::fprintf(stderr, "Failed to open file: %s\n", filename.data());
       ::fflush(stderr);
       ::abort();
     }
 
     auto filename_view = StringView(filename.data());
-    const std::string dir = filename_view.substr(0, filename_view.rfind('/')).ToString();
+    const std::string dir =
+        filename_view.substr(0, filename_view.rfind('/')).ToString();
     auto ret = 0;
 
 #ifdef KANON_ON_UNIX
     ret = ::mkdir(dir.data(), S_IRUSR);
 #elif defined(KANON_ON_WIN)
-    ret = ::_mkdir(dir.data()); 
+    ret = ::_mkdir(dir.data());
 #else
     // TODO Use c++17 file system API
-    ::fprintf("TODO: Call the directory creation function in specific platform!");
+    ::fprintf(
+        "TODO: Call the directory creation function in specific platform!");
     ::abort();
 #endif
     if (ret != 0) {
-      ::fprintf(stderr, "Failed to create a directory: %s\n", dir.data());  
+      ::fprintf(stderr, "Failed to create a directory: %s\n", dir.data());
       ::fflush(stderr);
       ::abort();
     }
   }
 
-  ::setbuffer(fp_, buf_, sizeof buf_);
+  setvbuf(fp_, buf_, _IOFBF, sizeof buf_);
 }
 
-AppendFile::~AppendFile() noexcept {
+AppendFile::~AppendFile() noexcept
+{
   Flush();
-  if (fp_)
-    ::fclose(fp_);
+  if (fp_) ::fclose(fp_);
 }
 
-void AppendFile::_Append(char const* data, size_t num) noexcept {
+void AppendFile::_Append(char const *data, size_t num) noexcept
+{
   size_t written = 0;
 
   do {
     size_t left = num - written;
     auto n = write(data + written, left);
-    
+
     if (n != left) {
       auto err = ferror(fp_);
       if (err)
         fprintf(stderr, "AppendFile::Append() failed %s\n", strerror_tl(err));
       break;
     }
-    
+
     written += n;
   } while (written != num);
 
--- kanon/log/append_file.h
+++ kanon/log/append_file.h
@@ -9,36 +9,44 @@
 
 namespace kanon {
 
-class AppendFile : noncopyable
-{
-public:
+class AppendFile : noncopyable {
+ public:
   explicit AppendFile(StringArg filename);
   ~AppendFile() noexcept;
-  
-  void Append(void const* data, size_t num) noexcept { _Append((char const*)data, num); }
+
+  void Append(void const *data, size_t num) noexcept
+  {
+    _Append((char const *)data, num);
+  }
 
   void Flush() noexcept
-  { ::fflush_unlocked(fp_); }
+  {
+#if defined(__GUNC__)
+    ::fflush_unlocked(fp_);
+#else
+    ::fflush(fp_);
+#endif
+  }
 
-  size_t writtenBytes() const noexcept
-  { return writtenBytes_; }
-  
-  FILE* fp() const noexcept { return fp_; }
+  size_t writtenBytes() const noexcept { return writtenBytes_; }
 
-private:
-  size_t write(char const* data, size_t num) noexcept
-  { return ::fwrite(data, 1, num, fp_); }
+  FILE *fp() const noexcept { return fp_; }
 
-  void _Append(char const* data, size_t num) noexcept;
-protected:
+ private:
+  size_t write(char const *data, size_t num) noexcept
+  {
+    return ::fwrite(data, 1, num, fp_);
+  }
+
+  void _Append(char const *data, size_t num) noexcept;
+
+ protected:
   char buf_[64 * 1024];
   // indicator of roll file
   size_t writtenBytes_;
-  FILE* fp_;
-
+  FILE *fp_;
 };
 
 } // namespace kanon
-
 
 #endif // KANON_APPENDFILE_H
