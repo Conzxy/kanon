788c770d984c176b7788ded05fe4fd3097c22aad
Conzxy <1967933844@qq.com>
2023-03-31 19:06:22 +0800

    NEW: Teardown 相关函数

--- kanon/util/init.cc
+++ kanon/util/init.cc
@@ -1,7 +1,14 @@
 #include "kanon/util/init.h"
+#include "kanon/log/logger.h"
 #include "kanon/thread/current_thread.h"
 
 void kanon::KanonCoreInitialize()
 {
   kanon::CurrentThread::MainThreadInitialize();
+  LOG_TRACE_KANON << "Kanon core resources initialization: OK";
+}
+
+void kanon::KanonCoreTeardown() KANON_NOEXCEPT
+{
+  LOG_TRACE_KANON << "Kanon core resources teardown: OK";
 }
