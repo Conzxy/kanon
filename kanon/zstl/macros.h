963845f9e4fab9460f67bae42e9ba94be4ac867d
Conzxy <1967933844@qq.com>
2023-03-31 19:09:51 +0800

    FIX: 避免宏冲突

--- kanon/zstl/macros.h
+++ kanon/zstl/macros.h
@@ -2,27 +2,37 @@
 #define _ZSTL_MACROS_H_
 
 #if __cplusplus >= 201103L
-#define CXX_STANDARD_11
-#endif 
+#  ifndef CXX_STANDARD_11
+#    define CXX_STANDARD_11
+#  endif
+#endif
 
 #ifndef CXX_STANDARD_11
-#define CXX_STANDARD_98 // this is maybe replaced by !CXX_STANDARD_11
-#endif 
+#  ifndef CXX_STANDARD_98
+#    define CXX_STANDARD_98 // this is maybe replaced by !CXX_STANDARD_11
+#  endif
+#endif
 
 #if __cplusplus >= 201402L
-#define CXX_STANDARD_14
+#  ifndef CXX_STANDARD_14
+#    define CXX_STANDARD_14
+#  endif
 #endif
 
 #if __cplusplus >= 201703L
-#define CXX_STANDARD_17
-#endif 
+#  ifndef CXX_STANDARD_17
+#    define CXX_STANDARD_17
+#  endif
+#endif
 
-#define STD_FORWARD(Args, args) std::forward<Args>(args)
+#ifndef STD_FORWARD
+#  define STD_FORWARD(Args, args) std::forward<Args>(args)
+#endif
 
 #ifdef CXX_STANDARD_14
-#define ZSTL_CONSTEXPR constexpr
+#  define ZSTL_CONSTEXPR constexpr
 #else
-#define ZSTL_CONSTEXPR KANON_INLINE
+#  define ZSTL_CONSTEXPR KANON_INLINE
 #endif
 
 #endif
