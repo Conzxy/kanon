53d0219a81208fbc8c8e0b6189e9447bde390c18
Conzxy <1967933844@qq.com>
2023-04-29 13:45:14 +0800

    feat(RawAny): Add `AnyCast2<T>()` to replace the `AnyCast<T>()`
    The `AnyCast<T>()` return `T*`, it is obscure to use.
    Tne `AnyCast2<T>()` return `T` is more friendly to user like `static_cast<T>()` or others.

--- kanon/util/raw_any.h
+++ kanon/util/raw_any.h
@@ -26,10 +26,24 @@ class RawAny final {
       zstl::negation<std::is_same<typename std::decay<T>::type, RawAny>>;
 
  public:
-  RawAny()
-    : obj_(nullptr)
+  RawAny() KANON_NOEXCEPT : obj_(nullptr) {}
+
+  ~RawAny() KANON_NOEXCEPT {}
+
+  RawAny(RawAny const &) = default;
+
+  RawAny(RawAny &&rhs) KANON_NOEXCEPT : obj_(rhs.obj_) { rhs.obj_ = nullptr; }
+
+  RawAny &operator=(RawAny const &) = default;
+  RawAny &operator=(RawAny &&rhs) KANON_NOEXCEPT
   {
+    if (&rhs != this) {
+      std::swap(rhs.obj_, obj_);
+    }
+    return *this;
   }
+
+  RawAny(void *obj) KANON_NOEXCEPT : obj_(obj) {}
 
   template <typename T, zstl::enable_if_t<AmbiguousCond<T>::value, char> = 0>
   RawAny(T &obj)
@@ -44,37 +58,83 @@ class RawAny final {
     return *this;
   }
 
+  /**
+   * In most case, this function isn't called.
+   * Unless you cast the argument to void* to force call it.
+   */
+  RawAny &operator=(void *obj) KANON_NOEXCEPT
+  {
+    obj_ = obj;
+    return *this;
+  }
+
   bool empty() const KANON_NOEXCEPT { return obj_ == nullptr; }
   void clear() KANON_NOEXCEPT { obj_ = nullptr; }
 
   void swap(RawAny &rhs) KANON_NOEXCEPT { std::swap(obj_, rhs.obj_); }
 
   template <typename V>
-  friend V *SafeAnyCast(RawAny from);
+  friend V *UnsafeAnyCast(RawAny from) KANON_NOEXCEPT;
 
   template <typename V>
-  friend V *UnsafeAnyCast(RawAny from);
+  friend V UnsafeAnyCast2(RawAny from) KANON_NOEXCEPT;
 
  private:
   void *obj_;
 };
 
+/* To RawAny, the cast must be unsafe, so the SafeAnyCast() and
+ * AnyCast() is a wrapper of UnsafeAnyCast(). */
+
 template <typename V>
-KANON_INLINE V *UnsafeAnyCast(RawAny from)
+KANON_INLINE V *UnsafeAnyCast(RawAny from) KANON_NOEXCEPT
 {
   return reinterpret_cast<V *>(from.obj_);
 }
 
 template <typename V>
-KANON_INLINE V *SafeAnyCast(RawAny from)
+KANON_INLINE V *SafeAnyCast(RawAny from) KANON_NOEXCEPT
+{
+  return UnsafeAnyCast<V>(from);
+}
+
+/**
+ * \param from The RawAny source
+ * \tparam V The casted destination type must be a Object type
+ * \return A pointer to Object type
+ *
+ * \warning
+ *  The return type and V is different.
+ *  If you don't like this, you should use \ref AnyCast2<V>()
+ */
+template <typename V>
+KANON_INLINE V *AnyCast(RawAny from) KANON_NOEXCEPT
 {
   return UnsafeAnyCast<V>(from);
 }
 
 template <typename V>
-KANON_INLINE V *AnyCast(RawAny from)
+KANON_INLINE V UnsafeAnyCast2(RawAny from) KANON_NOEXCEPT
 {
-  return UnsafeAnyCast<V>(from);
+  static_assert(std::is_pointer<V>::value,
+                "The template argument of AnyCast<> must be a pointer type");
+  return reinterpret_cast<V>(from.obj_);
+}
+
+template <typename V>
+KANON_INLINE V SafeAnyCast2(RawAny from) KANON_NOEXCEPT
+{
+  return UnsafeAnyCast2<V>(from);
+}
+
+/**
+ * The replacement of AnyCast<V>().
+ * The return type is same with the template parament
+ */
+template <typename V>
+KANON_INLINE V AnyCast2(RawAny from) KANON_NOEXCEPT
+{
+  return UnsafeAnyCast2<V>(from);
 }
 
 } // namespace kanon
