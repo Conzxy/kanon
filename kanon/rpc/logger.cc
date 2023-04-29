43fd95eec94d2e103a56bf4deee207d3f6429592
Conzxy <1967933844@qq.com>
2023-04-29 14:18:34 +0800

    refactor(rpc-logger): Replace `strcmp()` by `StrCastCompare()`.
    The `StrCastCompare()` in this case is redundant. The number is
    non-case.

--- kanon/rpc/logger.cc
+++ kanon/rpc/logger.cc
@@ -7,7 +7,7 @@ bool kanon::detail::g_kanon_protobuf_rpc_log_enable = true;
 static int InitKanonProtobufRpcLogEnable() KANON_NOEXCEPT
 {
   auto enable = ::getenv("KANON_PROTOBUF_RPC_LOG_ENABLE");
-  if (enable && !kanon::StrCaseCompare(enable, "0")) {
+  if (enable && !strcmp(enable, "0")) {
     kanon::detail::g_kanon_protobuf_rpc_log_enable = false;
   }
   return 0;
