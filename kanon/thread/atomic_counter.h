777231dbe7e9c9aa6fddff2b46cf53c3a3fa13fa
Conzxy <1967933844@qq.com>
2023-06-03 13:24:28 +0800

    fix(CountDownLatch): Ensure thread-safe
    Some method are not locked.

--- kanon/thread/atomic_counter.h
+++ kanon/thread/atomic_counter.h
@@ -9,38 +9,62 @@
 
 namespace kanon {
 
-template <typename T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
+template <typename T,
+          typename = typename std::enable_if<std::is_integral<T>::value>::type>
 class AtomicCounter : noncopyable {
   using Self = AtomicCounter;
 
  public:
-  explicit AtomicCounter(T const init_val = 0) : count_(init_val) {}
-
-  T operator=(T const val) KANON_NOEXCEPT {
-    Set(val);
-    return this->GetVal()();
+  explicit AtomicCounter(T const init_val = 0)
+    : count_(init_val)
+  {
   }
 
-  AtomicCounter(Self &&other) KANON_NOEXCEPT : count_(other.GetVal()()) {}
+  T operator=(T const val) KANON_NOEXCEPT
+  {
+    SetValue(val);
+    return this->GetValue();
+  }
 
-  Self &operator=(Self &&other) KANON_NOEXCEPT {
-    Set(other.GetVal());
+  AtomicCounter(Self &&other) KANON_NOEXCEPT : count_(other.GetValue()) {}
+
+  Self &operator=(Self &&other) KANON_NOEXCEPT
+  {
+    SetValue(other.GetValue());
     return *this;
   }
 
   T Reset() KANON_NOEXCEPT { count_.store(0, std::memory_order_relaxed); }
 
-  void SetValue(T const val) KANON_NOEXCEPT { count_.store(val, std::memory_order_relaxed); }
+  void SetValue(T const val) KANON_NOEXCEPT
+  {
+    count_.store(val, std::memory_order_relaxed);
+  }
 
-  T GetValue() const KANON_NOEXCEPT { return count_.load(std::memory_order_relaxed); }
+  T GetValue() const KANON_NOEXCEPT
+  {
+    return count_.load(std::memory_order_relaxed);
+  }
 
-  T Add(T const val) KANON_NOEXCEPT { return count_.fetch_add(val, std::memory_order_relaxed) + val; }
+  T Add(T const val) KANON_NOEXCEPT
+  {
+    return count_.fetch_add(val, std::memory_order_relaxed) + val;
+  }
 
-  T GetAndAdd(T const val) KANON_NOEXCEPT { return count_.fetch_add(val, std::memory_order_relaxed); }
+  T GetAndAdd(T const val) KANON_NOEXCEPT
+  {
+    return count_.fetch_add(val, std::memory_order_relaxed);
+  }
 
-  T Sub(T const val) KANON_NOEXCEPT { return count_.fetch_sub(val, std::memory_order_relaxed) - val; }
+  T Sub(T const val) KANON_NOEXCEPT
+  {
+    return count_.fetch_sub(val, std::memory_order_relaxed) - val;
+  }
 
-  T GetAndSub(T const val) KANON_NOEXCEPT { return count_.fetch_sub(val, std::memory_order_relaxed); }
+  T GetAndSub(T const val) KANON_NOEXCEPT
+  {
+    return count_.fetch_sub(val, std::memory_order_relaxed);
+  }
 
   operator T() const KANON_NOEXCEPT { return GetValue(); }
   T operator++() KANON_NOEXCEPT { return Add(1); }
@@ -55,6 +79,6 @@ class AtomicCounter : noncopyable {
 using AtomicCounter32 = AtomicCounter<uint32_t>;
 using AtomicCounter64 = AtomicCounter<uint64_t>;
 
-}  // namespace kanon
+} // namespace kanon
 
 #endif
